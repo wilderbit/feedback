96c0056d3945c3a8d50e50854a14c5440ee76879
AbrarNitk <abrar.nitk@gmail.com>
2019-07-17 08:29:20 +0530

    customized user model

--- README.md
+++ README.md
@@ -16,4 +16,8 @@ latest python 3
 install django
 Virtual Environment(feedback) using pyenv (or python3 -m venv <env_name>)
 
-[Access Privileges](https://tableplus.io/blog/2018/04/postgresql-how-to-grant-access-to-users.html)
+Postgres [Access Privileges](https://tableplus.io/blog/2018/04/postgresql-how-to-grant-access-to-users.html)
+
+```django-admin startproject feedback```
+``` cstartapp users ```
+``` python manage.py sqlmigrate <app_name> <migration>```
--- src/users/models.py
+++ src/users/models.py
@@ -6,4 +6,77 @@ from encrypted_id.models import EncryptedIDModel, EncryptedIDManager
 # Create your models here.
 # Customized User model.
 
-# class UserManager(BaseUserManager, EncryptedIDManager):
+class UserManager(BaseUserManager, EncryptedIDManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(email, password=password)
+        user.staff = True
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password
+        """
+        user = self.create_user(email, password=password)
+        user.staff = True
+        user.admin = True
+        user.save()
+        return user
+
+    def get_by_natural_key(self, value):
+        return self.get(**{self.model.USERNAME_FIELD: value})
+
+
+class User(AbstractBaseUser, EncryptedIDModel):
+    first_name = models.CharField(max_length=100, blank=True, default="")
+    last_name = models.CharField(max_length=100, blank=True, default="")
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    staff = models.BooleanField(
+        default=False,
+        help_text="""
+            People from company
+        """,
+    )
+    admin = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # Email and password are required default
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        """Does User have a specific permission"""
+        return True
+
+    def has_module_perms(self, app_label):
+        """Does user have permission to view the app `app_label`?"""
+        return True
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.is_active
