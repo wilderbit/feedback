31817aa270ebcbdfde69e52e7bc9b15f4317f0c0
AbrarNitk <abrar.nitk@gmail.com>
2019-08-26 09:30:17 +0530

    random commit

--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
 # Create your views here.
 
 
 def home(req):
     return render(request=req, template_name="index.html")
+
+
+def login_user(req):
+    logout(req)
+    if req.POST:
+        email = req.POST["email"]
+        password = req.POST["password"]
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            if user.is_active:
+                login(req, user)
+                pass
